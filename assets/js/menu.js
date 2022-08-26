82caffc084539078fa0aec5138f7f35939ab5c57
Stefan Wagner <tinko26@outlook.com>
2022-08-26 18:08:31 +0200

    Update.

---
+++ assets/js/menu.js
@@ -0,0 +1,7 @@
+function menuOpen() {
+    document.getElementsByClassName("menu")[0].style.height = "100%";
+}
+
+function menuClose() {
+    document.getElementsByClassName("menu")[0].style.height = "0%";
+}

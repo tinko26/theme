6209024c21264c86d59f2a91332a6c245b0af44b
Stefan Wagner <tinko26@outlook.com>
2022-09-05 16:24:59 +0200

    Update.

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -3,16 +3,16 @@ function highlight() {
 		$(this).html(
 			$(this)
 			.html()
-			.replaceAll(' defined ', ' <span class="cp-k">defined</span> ')
 			.replaceAll('#define ', '<span class="cp-k">#define</span> ')
 			.replaceAll('#elif ', '<span class="cp-k">#elif</span> ')
-			.replaceAll('#else ', '<span class="cp-k">#else</span> ')
-			.replaceAll('#endif ', '<span class="cp-k">#endif</span> ')
+			.replaceAll('#else', '<span class="cp-k">#else</span>')
+			.replaceAll('#endif', '<span class="cp-k">#endif</span>')
 			.replaceAll('#if ', '<span class="cp-k">#if</span> ')
 			.replaceAll('#ifdef ', '<span class="cp-k">#ifdef</span> ')
 			.replaceAll('#ifndef ', '<span class="cp-k">#ifndef</span> ')
 			.replaceAll('#include ', '<span class="cp-k">#include</span> ')
 			.replaceAll('#include_next ', '<span class="cp-k">#include_next</span> ')
+			.replaceAll('defined ', '<span class="cp-k">defined</span> ')
 		);
 	});
 }

e9fe63ba5de61e8f7888e62388e4185c0f029c48
Stefan Wagner <tinko26@outlook.com>
2022-09-08 16:45:12 +0200

    Update.

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,12 +1,5 @@
 function content() {
-	$(
-		".content-main h1 code, " + 
-		".content-main h2 code, " + 
-		".content-main h3 code, " + 
-		".content-main h4 code, " + 
-		".content-main h5 code, " + 
-		".content-main h6 code"
-	).each(function() {
+	$(".content-main :not(pre) > code").each(function() {
 		$(this).html(
 			$(this)
 			.html()

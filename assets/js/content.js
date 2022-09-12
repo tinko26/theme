6ca797ad49bf94e0309fb356aeef8445db073724
Stefan Wagner <tinko26@outlook.com>
2022-09-12 19:57:54 +0200

    Update.

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,9 +1,8 @@
 function content() {
-	$(":not(pre) > code").each(function() {
-		$(this).html(
-			$(this)
-			.html()
-			.replace(/_/g, "_<wbr/>")
-		);
-	});
+	$(".content-main h1 > code").each(function() { $(this).html($(this).html().replace(/_/g, "_<wbr/>")); });
+	$(".content-main h2 > code").each(function() { $(this).html($(this).html().replace(/_/g, "_<wbr/>")); });
+	$(".content-main h3 > code").each(function() { $(this).html($(this).html().replace(/_/g, "_<wbr/>")); });
+	$(".content-main h4 > code").each(function() { $(this).html($(this).html().replace(/_/g, "_<wbr/>")); });
+	$(".content-main h5 > code").each(function() { $(this).html($(this).html().replace(/_/g, "_<wbr/>")); });
+	$(".content-main h6 > code").each(function() { $(this).html($(this).html().replace(/_/g, "_<wbr/>")); });
 }
